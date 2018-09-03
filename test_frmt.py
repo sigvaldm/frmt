1237b2cbadade457e607ebd3ce8f5d92fc0b27ea
sigvaldm <sigvaldm@fys.uio.no>
2018-09-03 20:19:59 +0200

    Added py.test

---
+++ test_frmt.py
@@ -0,0 +1,53 @@
+from frmt import *
+from numpy import nan
+
+def test_fit_text():
+    assert fit_text('abc', 5) == 'abc  '
+    assert fit_text('abc', 5, '<') == 'abc  '
+    assert fit_text('abc', 5, '^') == ' abc '
+    assert fit_text('abc', 5, '>') == '  abc'
+    assert fit_text('abc', 2) == '..'
+    assert fit_text('abcdef', 5) == 'ab...'
+    assert fit_text('abcdef', 5, suffix='!', align='>') == 'abcd!'
+    # TBD: Add test testing terminal width?
+
+def test_format_time_special():
+    assert format_time('text') == 'text'
+    assert format_time(nan) == '-'
+    assert format_time(0) == '0'
+
+def test_format_time_lower_limits():
+    assert format_time(4e-10) == '0'
+    assert format_time(5e-10) == '1ns'
+    assert format_time(-4e-10) == '0'
+    assert format_time(-5e-10) == '-1ns'
+
+def test_format_time_scaling():
+    assert format_time(1e-9) == '1ns'
+    assert format_time(1e-8) == '10ns'
+    assert format_time(1e-7) == '100ns'
+    assert format_time(1e-6) == '1.00us'
+    assert format_time(1e-5) == '10.0us'
+    assert format_time(1e-4) == '100us'
+    assert format_time(1e-3) == '1.00ms'
+    assert format_time(1e-2) == '10.0ms'
+    assert format_time(1e-1) == '100ms'
+    assert format_time(1e0) == '1.00s'
+    assert format_time(1e1) == '10.0s'
+    assert format_time(59) == '59.0s'
+    assert format_time(60) == '1:00'
+    assert format_time(60**2) == '1:00:00'
+    assert format_time(24*60**2) == '1:00:00:00'
+
+def test_format_time_rounding():
+    assert format_time(1.0049999) == '1.00s'
+    assert format_time(1.0050001) == '1.01s'
+    assert format_time(-1.0049999) == '-1.00s'
+    assert format_time(-1.0050001) == '-1.01s'
+
+def test_format_time_mode():
+    assert format_time(60,'small') == '60.0s'
+    assert format_time(99,'small') == '99.0s'
+    assert format_time(100,'small') == '100s'
+    assert format_time(0.1,'large') == '0'
+    assert format_time(10,'large') == '10'
