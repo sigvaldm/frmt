87874776243d0b99898b01ad45548cb84f99e8b9
sigvaldm <sigvaldm@fys.uio.no>
2018-09-19 21:40:18 +0200

    Build errors

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(name='frmt',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries'
         ]
      )
 
